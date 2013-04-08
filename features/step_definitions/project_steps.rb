5ef87193797a0aabc8b537a92b9d2214cdedfe13
Robert Kerr <robkerr87@gmail.com>
2013-04-08 17:19:48 +1000

    Implemented features for displaying a list of relevant                        tickets for projects and viewing particular tickets

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -2,36 +2,39 @@ Given(/^I am on the homepage$/) do
   visit "/"
 end
 
-When(/^I follow "(.*?)"$/) do |link|
+When(/^I follow "([^"]*)"$/) do |link|
   click_link link
 end
 
-When(/^I fill in "(.*?)" with "(.*?)"$/) do |form_item, input|
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |form_item, input|
   fill_in form_item, :with => input
 end
 
-When(/^I press "(.*?)"$/) do |button|
+When(/^I press "([^"]*)"$/) do |button|
   click_button button
 end
 
-Then(/^I should see "(.*?)"$/) do |text|
-  msg = "But I don't. "
-  assert (has_text? text), msg
+Then(/^I should see "([^"]*)"$/) do |text|
+  should have_content(text)
 end
 
-Then(/^I should not see "(.*?)"$/) do |text|
-  msg = "But I do. "
-  assert !(has_text? text), msg
+Then(/^I should see "([^"]*)" within "([^"]*)"$/) do |text,obj|
+  within(obj) do
+    should have_content(text)
+  end
 end
 
-Then(/^I should be on the project page for "(.*?)"$/) do |name|
+Then(/^I should not see "([^"]*)"$/) do |text|
+  should_not have_content(text)
+end
+
+Then(/^I should be on the project page for "([^"]*)"$/) do |name|
   right_page = current_url == project_url(Project.find_by_name!(name))
   msg = "I am instead on the page: " + current_url + ". "
   assert right_page, msg
 end
 
-Given(/^there is a project called "(.*?)"$/) do |name|
-  create(:project, name: name)
+Given(/^there is a project called "([^"]*)"$/) do |name|
+  @project = create(:project, name: name)
 end
-
 
